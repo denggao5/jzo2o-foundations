449ee07915f234b8571e2eff4feedf27d3d11666
gaodeng <952335421@qq.com>
2026-09-06 18:56:23 +0800

    运营端区域服务下分页查询接口Service层开发，PageHelper分页查询以及单元测试

---
+++ src/main/java/com/jzo2o/foundations/service/impl/ServeServiceImpl.java
@@ -0,0 +1,27 @@
+package com.jzo2o.foundations.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.jzo2o.common.model.PageResult;
+import com.jzo2o.foundations.mapper.ServeMapper;
+import com.jzo2o.foundations.model.domain.Serve;
+import com.jzo2o.foundations.model.dto.request.ServePageQueryReqDTO;
+import com.jzo2o.foundations.model.dto.response.ServeResDTO;
+import com.jzo2o.foundations.service.IServeService;
+import com.jzo2o.mysql.utils.PageHelperUtils;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ServeServiceImpl extends ServiceImpl<ServeMapper, Serve> implements IServeService {
+
+    /**
+     * 分页查询服务列表
+     * @param servePageQueryReqDTO 查询条件
+     * @return 分页列表
+     */
+    @Override
+    public PageResult<ServeResDTO> page(ServePageQueryReqDTO servePageQueryReqDTO) {
+        //调用mapper查询数据，这里由于继承了ServiceImpl<ServeMapper, Serve>，使用baseMapper相当于使用ServeMapper
+        PageResult<ServeResDTO> serveResDTOPageResult = PageHelperUtils.selectPage(servePageQueryReqDTO, () -> baseMapper.queryServeListByRegionId(servePageQueryReqDTO.getRegionId()));
+        return serveResDTOPageResult;
+    }
+}
